3cb0d9b8969591638c4892ee730c156607e555dc
wuwo <wuwo@wacai.com>
2017-05-26 21:11:38 +0800

    re initialize Xproxy project

---
+++ src/main/java/xproxy/Xproxy.java
@@ -0,0 +1,12 @@
+package xproxy;
+
+/**
+ * reverse proxy implement in java
+ * 
+ * @author wenK712@gmail.com
+ */
+public class Xproxy {
+	public static void main(String[] args) {
+		System.out.println("This is Xproxy");
+	}
+}
